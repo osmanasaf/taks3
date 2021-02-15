6be55ef627699a09ab5ec85ae290b753447fbbbf
Osman TATAR <osmaan.tatar@gmail.com>
2021-02-15 10:56:03 +0300

    Swagger baglandı

--- src/main/java/com/example/codefirst/task3/Task3Application.java
+++ src/main/java/com/example/codefirst/task3/Task3Application.java
@@ -2,12 +2,20 @@ package com.example.codefirst.task3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+
 public class Task3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Task3Application.class, args);
 	}
+
 
 }
